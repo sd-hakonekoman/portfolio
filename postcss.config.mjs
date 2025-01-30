484f055f8631df04fdbaab3dfcd910cb7af4db6a
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2025-01-30 15:06:35 +0900

    Tailwind CSSのバージョンアップ、環境変数の追加

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,7 @@
-import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import sortMediaQueries from "postcss-sort-media-queries";
 import postcssCustomMedia from "postcss-custom-media";
 
 export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-    sortMediaQueries(),
-    postcssCustomMedia(),
-  ],
+  plugins: [autoprefixer(), sortMediaQueries(), postcssCustomMedia()],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default {
   build: {
     cssMinify: false, // CSSの圧縮を無効化
   },
+  plugins: [tailwindcss()],
 };
