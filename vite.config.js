6937fc9a887c795ddf376ec42a9ca1e3903155ac
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2025-02-07 17:46:31 +0900

    build時にJSのconsole.logを出力しないように設定

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  root: "src",
-  envDir: "../",
-  base: "./",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true, // ビルド前に出力先ディレクトリをクリーンアップ
-    cssMinify: false, // CSSの圧縮を無効化
-  },
-  server: {
-    host: true,
-  },
+export default defineConfig(({ command }) => {
+  // 共通の設定
+  const commonSettings = {
+    root: "src",
+    envDir: "../",
+    base: "./",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true, // ビルド前に出力先ディレクトリをクリーンアップ
+      cssMinify: false, // CSSの圧縮を無効化
+    },
+    server: {
+      host: true,
+    },
+  };
+
+  // 追加の設定
+  let addSettings = {};
+
+  if (command === "build") {
+    // build 固有の設定
+    addSettings = {
+      esbuild: {
+        drop: ["console", "debugger"],
+      },
+    };
+  }
+
+  return { ...commonSettings, ...addSettings };
 });
