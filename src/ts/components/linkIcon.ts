dfdbe7f36bf14fc8114d5bb7a743cf95b3e11597
Abe <sd-hakonekoman@users.noreply.github.com>
2026-03-03 15:24:13 +0900

    ヘッダーにGithubへのリンクを追加 close #24

--- src/ts/components/linkIcon.ts
+++ src/ts/components/linkIcon.ts
@@ -1,7 +1,8 @@
+// 外部リンクアイコンをつけないaタグに対しては、data-blank-icon="false"をつける
 import { FONT_AWESOME_ARROUPICON } from "../constants/icon";
 
 export const LinkIcon = () => {
-  const aBlankEls = document.querySelectorAll("a[target='_blank']");
+  const aBlankEls = document.querySelectorAll("a[target='_blank']:not([data-blank-icon='false'])");
   aBlankEls.forEach((el) => {
     el.insertAdjacentHTML("beforeend", FONT_AWESOME_ARROUPICON);
   });
