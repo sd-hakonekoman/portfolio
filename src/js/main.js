a8544836c1213f7f6b280657a8c6751e77d1f4fc
Abe <sd-hakonekoman@users.noreply.github.com>
2026-06-08 21:36:29 +0900

    HTMLからAstroに変更

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 import "../scss/style.scss";
 import { LinkIcon } from "./components/linkIcon.js";
 import { Menu } from "./components/menu.js";
-import { Skills } from "./components/skills.js";
 import { Header } from "./components/header.js";
 
-Skills();
 LinkIcon();
 Menu();
 Header();
